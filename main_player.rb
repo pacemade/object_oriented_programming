515f51987d1d43499b21b3a6f2a3b50e46e6490e
Danny Chang <dannychachang@gmail.com>
2017-12-15 19:03:28 -0500

    Make level up, collect treasure, set player and 20 times test

---
+++ main_player.rb
@@ -0,0 +1,8 @@
+require './player.rb'
+
+jun_tao = Player.new(0, 0 , 0)
+
+20.times do
+  jun_tao.collect_treasure
+  p jun_tao
+end
---
+++ player.rb
@@ -0,0 +1,21 @@
+class Player
+
+    def initialize(gold_coins, health_points, lives)
+      @lives = 5
+      @gold_coins = 0
+      @health_points = 10
+    end
+
+    def level_up
+      @lives += 1
+    end
+
+    def collect_treasure
+      @gold_coins += 1
+      if @gold_coins % 10 == 0
+          level_up
+      end
+    end
+
+
+end
